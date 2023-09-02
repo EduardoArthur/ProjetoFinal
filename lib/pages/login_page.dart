49f82785cf42519fe37619f91ff077c923205fae
EduardoArthur <dudullllima@gmail.com>
2023-09-02 18:04:31 -0300

    ajustes login_page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -22,6 +22,7 @@ class _LoginPageState extends State<LoginPage> {
   final ongService = OngService();
 
   bool isLogin = true;
+  bool rememberMe = false;
   late String titulo;
   late String actionButton;
   late String toggleButton;
@@ -115,117 +116,14 @@ class _LoginPageState extends State<LoginPage> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text(
-                  titulo,
-                  style: const TextStyle(
-                    fontSize: 35,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: -1.5,
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(24),
-                  child: TextFormField(
-                    controller: email,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Email',
-                    ),
-                    keyboardType: TextInputType.emailAddress,
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Informe o email corretamente!';
-                      }
-                      return null;
-                    },
-                  ),
-                ),
-                Padding(
-                  padding:
-                  const EdgeInsets.symmetric(vertical: 12.0, horizontal: 24.0),
-                  child: TextFormField(
-                    controller: senha,
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Senha',
-                    ),
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Informa sua senha!';
-                      } else if (value.length < 6) {
-                        return 'Sua senha deve ter no mínimo 6 caracteres';
-                      }
-                      return null;
-                    },
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(24.0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      if (formKey.currentState!.validate()) {
-                        if (isLogin) {
-                          login();
-                        } else {
-                          registrar();
-                        }
-                      }
-                    },
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: (loading)
-                          ? [
-                        const Padding(
-                          padding: EdgeInsets.all(16),
-                          child: SizedBox(
-                            width: 24,
-                            height: 24,
-                            child: CircularProgressIndicator(
-                              color: Colors.white,
-                            ),
-                          ),
-                        )
-                      ]
-                          : [
-                        Icon(Icons.check),
-                        Padding(
-                          padding: EdgeInsets.all(16.0),
-                          child: Text(
-                            actionButton,
-                            style: TextStyle(fontSize: 20),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                TextButton(
-                  onPressed: () => setFormAction(!isLogin),
-                  child: Text(toggleButton),
-                ),
-                TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => ReportLostAnimalPage(),
-                      ),
-                    );
-                  },
-                  child: Text(loginAnonimo),
-                ),
-                TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => CadastroOngPage(),
-                      ),
-                    );
-                  },
-                  child: Text(loginOng),
-                ),
+                showTitle(),
+                emailController(),
+                passwordController(),
+                loginButtonController(),
+                registerButton(),
+                botaoDenunciaAnonima(),
+                registerOngButton(),
+                // rememberMeCheckBox(),
               ],
             ),
           ),
@@ -233,4 +131,153 @@ class _LoginPageState extends State<LoginPage> {
       ),
     );
   }
+
+  Widget showTitle(){
+    return Text(
+      titulo,
+      style: const TextStyle(
+        fontSize: 35,
+        fontWeight: FontWeight.bold,
+        letterSpacing: -1.5,
+      ),
+    );
+  }
+
+  Widget emailController(){
+    return Padding(
+      padding: EdgeInsets.all(24),
+      child: TextFormField(
+        controller: email,
+        decoration: const InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'Email',
+        ),
+        keyboardType: TextInputType.emailAddress,
+        validator: (value) {
+          if (value!.isEmpty) {
+            return 'Informe o email corretamente!';
+          }
+          return null;
+        },
+      ),
+    );
+  }
+
+  Widget passwordController(){
+    return Padding(
+      padding:
+      const EdgeInsets.symmetric(vertical: 12.0, horizontal: 24.0),
+      child: TextFormField(
+        controller: senha,
+        obscureText: true,
+        decoration: const InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'Senha',
+        ),
+        validator: (value) {
+          if (value!.isEmpty) {
+            return 'Informa sua senha!';
+          } else if (value.length < 6) {
+            return 'Sua senha deve ter no mínimo 6 caracteres';
+          }
+          return null;
+        },
+      ),
+    );
+  }
+
+  Widget loginButtonController(){
+    return Padding(
+      padding: EdgeInsets.all(24.0),
+      child: ElevatedButton(
+        onPressed: () {
+          if (formKey.currentState!.validate()) {
+            if (isLogin) {
+              login();
+            } else {
+              registrar();
+            }
+          }
+        },
+        child: loginButton(),
+      ),
+    );
+  }
+
+  Widget loginButton(){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: (loading)
+          ? [
+        const Padding(
+          padding: EdgeInsets.all(16),
+          child: SizedBox(
+            width: 24,
+            height: 24,
+            child: CircularProgressIndicator(
+              color: Colors.white,
+            ),
+          ),
+        )
+      ]
+          : [
+        Icon(Icons.check),
+        Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Text(
+            actionButton,
+            style: TextStyle(fontSize: 20),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget registerButton(){
+    return TextButton(
+      onPressed: () => setFormAction(!isLogin),
+      child: Text(toggleButton),
+    );
+  }
+
+  Widget botaoDenunciaAnonima(){
+    return TextButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ReportLostAnimalPage(),
+          ),
+        );
+      },
+      child: Text(loginAnonimo),
+    );
+  }
+
+  Widget registerOngButton(){
+    return TextButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => CadastroOngPage(),
+          ),
+        );
+      },
+      child: Text(loginOng),
+    );
+  }
+
+  Widget rememberMeCheckBox(){
+    return CheckboxListTile(
+      title: Text("Lembrar de Mim"),
+      value: rememberMe,
+      onChanged: (value) {
+        setState(() {
+          rememberMe = value!;
+        });
+      },
+    );
+  }
+
 }
